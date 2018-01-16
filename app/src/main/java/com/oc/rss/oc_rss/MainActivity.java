87ce887f5f7021d2c940af88804f38817889acb2
haja91150 <haja91150@gmail.com>
2018-01-16 14:45:06 +0100

    Exo click bouton

--- app/src/main/java/com/oc/rss/oc_rss/MainActivity.java
+++ app/src/main/java/com/oc/rss/oc_rss/MainActivity.java
@@ -1,13 +1,32 @@
 package com.oc.rss.oc_rss;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button myButton;
+    Context myContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        myContext = this;
         setContentView(R.layout.activity_main);
+        myButton = (Button)findViewById(R.id.btnClickHere);
+        myButton.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(myContext, "CA MARCHE !", Toast.LENGTH_SHORT).show();
+                Log.i("haja", "Ca marche !!!");
+            }
+        });
     }
 }
