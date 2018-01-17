b411505b2fe1797a05e6704b0a1e802c17dab518
haja91150 <haja91150@gmail.com>
2018-01-17 17:37:47 +0100

    Ajout menu dans ActionBar

--- app/src/main/java/com/oc/rss/oc_rss/MainActivity.java
+++ app/src/main/java/com/oc/rss/oc_rss/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         myContext = this;
         setContentView(R.layout.activity_main);
+        setTitle("Tuto Appli OpenClassRoom");
         myButton = (Button)findViewById(R.id.btnClickHere);
         myButton.setOnClickListener(new OnClickListener() {
 
@@ -33,5 +36,27 @@ public class MainActivity extends AppCompatActivity {
                 MainActivity.this.finish(); // we closed this activity after launch the new one
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_add:
+                Toast.makeText(MainActivity.this, "ADD !", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_edit:
+                Toast.makeText(MainActivity.this, "EDIT !", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_delete:
+                Toast.makeText(MainActivity.this, "DELETE !", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
